78c255af23236cede34dc06c1effbbe854c2cae2
jofforka <54028942+jofforka@users.noreply.github.com>
2026-07-08 09:05:08 +0100

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,7 @@
+window.TIMZY_CONFIG = {
+  whatsapp: '2340000000000',
+  productFormUrl: 'https://forms.google.com/',
+  productSheetUrl: 'https://docs.google.com/spreadsheets/',
+  sheetCsvUrl: '',
+  brandName: 'Timzy Fashion'
+};
