9fa8515cfc055e73a0bd426b800d7b6d8ebd6e74
jofforka <54028942+jofforka@users.noreply.github.com>
2026-07-14 12:57:20 +0100

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,1 +1,8 @@
-/* Firebase setup will be activated in admin phase. Public site works without Firebase. */
+const firebaseConfig = {
+  apiKey: "AIzaSyBCizR30KTtGXwlelD4Qxdu9IHJdPm-IlU",
+  authDomain: "timzy-fashion-os.firebaseapp.com",
+  projectId: "timzy-fashion-os",
+  storageBucket: "timzy-fashion-os.firebasestorage.app",
+  messagingSenderId: "515655826693",
+  appId: "1:515655826693:web:4085b86651f39ffa03cb6c"
+};
